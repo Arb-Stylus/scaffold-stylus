52d720a5ffbbf05052d2aaa628e32de2623f066b
tu11aa <thanhtuhr01@gmail.com>
2025-10-14 20:52:01 +0700

    chore: remove counter contract

--- packages/stylus/scripts/deploy.ts
+++ packages/stylus/scripts/deploy.ts
@@ -36,19 +36,6 @@ export default async function deployScript(deployOptions: DeployOptions) {
     ...deployOptions,
   });
 
-  await deployStylusContract({
-    contract: "counter",
-    ...deployOptions,
-  });
-
-  // EXAMPLE: Deploy to Orbit Chains, uncomment to try
-  // await deployStylusContract({
-  //   contract: "counter",
-  //   constructorArgs: [100],
-  //   isOrbit: true,
-  //   ...deployOptions,
-  // });
-
   // EXAMPLE: Deploy your contract with a custom name, uncomment to try
   // await deployStylusContract({
   //   contract: "your-contract",
